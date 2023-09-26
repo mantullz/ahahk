c4953280e153e6f6bd5ede296298cc94625be006
mantullz <36762688+mantullz@users.noreply.github.com>
2023-09-26 19:46:21 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -40,7 +40,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/muhamadparizan/kebenaran/main/ipdibenarkan | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/mantullz/kebenaran/main/ipdibenarkan | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
 echo -e "${green}Tahniah! Anda Dibenarkan menggunakan Script Ctech Didik...${NC}"
